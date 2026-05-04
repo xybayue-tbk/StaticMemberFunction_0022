737d85bcb74d0e93bc7fc28896100ea1b351dea5
xybayue-tbk <xybayue@gmail.com>
2026-05-04 12:53:55 +0700

    Menambahkan member dan this

--- this.cpp
+++ this.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 class buku{
+    string judul;
 
+    public:
+    string setget(string judul) {
+        this->judul = judul; // nilai parameter 'judul' untuk member variabel
+        return this->judul; // return variabel judul
+    }
 };
