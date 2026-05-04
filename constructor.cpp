b1ef788b86ba4d5c386dad2b57dbce6f7eeb164c
xybayue-tbk <xybayue@gmail.com>
2026-05-04 12:42:01 +0700

    Pembuatan Object

--- constructor.cpp
+++ constructor.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
+class mahasiswa{
 public:
     mahasiswa(); // Constructor
 };
 
-mahasiswa::mahasiswa() {
+mahasiswa::mahasiswa(){
     cout << "Constructor Terpanggil" << endl; // Definisi Constructor
 };
+
+int main(){
+    mahasiswa mhs; // Pembuatan Object
+    return 0;
+}
