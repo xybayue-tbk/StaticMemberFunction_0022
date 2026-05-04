d6a5f675b7f2e5d3e23ea6ce275ef6ad263682e4
xybayue-tbk <xybayue@gmail.com>
2026-05-04 13:29:14 +0700

    Menambahkan langkah" nya

--- Destructor.cpp
+++ Destructor.cpp
@@ -1,2 +1,46 @@
 #include <iostream>
 using namespace std;
+
+class angka
+{
+    private:
+    int *arr;
+    int panjang;
+
+    public:
+    angka(int);  // Constructor
+    ~angka();     // Destructor
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i) {
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << "=" << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 0; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
