3f910f0eb48bd2f88adcc90d3edeadc60712a66e
xybayue-tbk <xybayue@gmail.com>
2026-05-04 13:25:51 +0700

    main progam

--- Constructor_Overloading.cpp
+++ Constructor_Overloading.cpp
@@ -34,3 +34,16 @@ void mahasiswa :: cetak() {
     cout << endl << "NIM = " << nim << endl;
     cout << "Nama = " << nama << endl;
 }
+
+int main(){
+    mahasiswa mhs1 (102030);                  // Hanya Nim
+    mahasiswa mhs2 ("Andi");                  // Hanya Nama
+    mahasiswa mhs3 (010203, "Santi");          // Hanya Nim dan Nama
+
+    //Tampilkan nilai
+    cout <<"Mahasiswa 1:"; mhs1.cetak();
+    cout <<"Mahasiswa 2:"; mhs2.cetak();
+    cout <<"Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
