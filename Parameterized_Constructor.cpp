fd967799f671409c5a014f4c6e09807806fd6017
xybayue-tbk <xybayue@gmail.com>
2026-05-04 13:17:33 +0700

    Memanggil Constructor dengan parameter

--- Parameterized_Constructor.cpp
+++ Parameterized_Constructor.cpp
@@ -13,3 +13,8 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "NIM: " << nim << endl;
     cout << "Nama: " << nama << endl;
 }
+
+int main() {
+    mahasiswa mhs(12345, "Pascal"); // Memanggil Constructor dengan parameter
+    return 0;
+}
