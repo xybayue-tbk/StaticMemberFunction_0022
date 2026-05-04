b69fa1876d3ad8adcbc1e508538e1cfd7bf62a66
xybayue-tbk <xybayue@gmail.com>
2026-05-04 13:30:46 +0700

    Menambahkan Main Progam

--- Destructor.cpp
+++ Destructor.cpp
@@ -44,3 +44,12 @@ void angka::isiData()
     }
     cout << endl;
 }
+
+int main()
+{
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);  
+    delete ptrBelajarcpp;     
+
+    return 0;
+}
