c350861a9891c123faff9c68eb5f9181a2830b40
xybayue-tbk <xybayue@gmail.com>
2026-05-04 13:16:51 +0700

    Definisi Constructor dengan parameter

--- Parameterized_Constructor.cpp
+++ Parameterized_Constructor.cpp
@@ -6,3 +6,10 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama); //Constructor dengan parameter
 };
+
+// Definisi Constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor dengan parameter Terpanggil" << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Nama: " << nama << endl;
+}
